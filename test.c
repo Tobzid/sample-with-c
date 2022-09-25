7e2d16b55ef79c1ff113a8b1ced2caab7fccad42
Tobzid <Tobzid2013@gmail.com>
2022-09-25 16:22:45 +0100

    just to test this

---
+++ test.c
@@ -0,0 +1,15 @@
+/* This is just to test run github on vscode */
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+char new;
+char litenew[] = "my name is Tolu";
+
+strcpy(new, litenew);
+
+printf("new now contains %s", new);
+return 0;
+}
